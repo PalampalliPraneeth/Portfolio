6f63d38f0df40d079266059cc4f0bfabc547062b
praneeth reddy <praneeth.reddy@tudip.com>
2024-01-22 01:02:10 -0500

    topbar & hamburger is completed

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import "./menu.scss";
 
-export default function Menu() {
+export default function Menu({menuOpen,setMenuOpen}) {
   return (
-    <div className='menu'>
+    <div className={'menu ' + (menuOpen && "active")}>
         <ul>
-            <li>
+            <li onClick={()=>setMenuOpen(false)}>
                 <a href='#intro'>Intro</a>
             </li>
-            <li>
+            <li onClick={()=>setMenuOpen(false)}>
                 <a href="#portfolio">Portfolio</a>
             </li>
-            <li>
+            <li onClick={()=>setMenuOpen(false)}>
                 <a href="#works">Works</a>
             </li>
-            <li>
+            <li onClick={()=>setMenuOpen(false)}>
                 <a href="#testimonials">Testimonials</a>
             </li>
-            <li>
+            <li onClick={()=>setMenuOpen(false)}>
                 <a href="#contact">Contact</a>
             </li>
         </ul>
--- src/components/topbar.js/Topbar.jsx
+++ src/components/topbar.js/Topbar.jsx
@@ -5,7 +5,7 @@ import {Person, Mail} from "@material-ui/icons";
 
 export default function Topbar({menuOpen,setMenuOpen}) {
   return (
-    <div className={'topbar ' + (menuOpen && "active")}id='topbar'>
+    <div className={'topbar ' + (menuOpen && "active")} id='topbar'>
        <div className="wrapper">
         <div className="left">
             <a href='#works' className='logo'>Portfolio</a>
