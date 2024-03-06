ec43c06ab792dcae2d8f5fba38e350db55e5fbf1
praneeth reddy <praneeth.reddy@tudip.com>
2024-03-05 20:25:31 -0500

    trying for deployment

--- src/components/socialMediaIcons/MediaIcons.jsx
+++ src/components/socialMediaIcons/MediaIcons.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Facebook, LinkedIn, Instagram, GitHub } from '@material-ui/icons';
+// import { makeStyles } from '@material-ui/core/styles';
+// import { Facebook, LinkedIn, Instagram, GitHub } from '@material-ui/icons';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: 'grey',
-    borderRadius: '4%',
-    position: 'fixed',
-    top: '50%',
-    left: theme.spacing(2),
-    transform: 'translateY(-50%)',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 999,
-  },
-  icon: {
-    margin: theme.spacing(1),
-    fontSize: '2rem',
-    color: '#fff',
-    cursor: 'pointer',
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     backgroundColor: 'grey',
+//     borderRadius: '4%',
+//     position: 'fixed',
+//     top: '50%',
+//     left: theme.spacing(2),
+//     transform: 'translateY(-50%)',
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     zIndex: 999,
+//   },
+//   icon: {
+//     margin: theme.spacing(1),
+//     fontSize: '2rem',
+//     color: '#fff',
+//     cursor: 'pointer',
+//   },
+// }));
 
 const SocialMediaIcons = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    // <div className={classes.root}>
+    <div>
       <a href="https://www.linkedin.com/in/praneethpalampalli/" target="_blank" rel="noopener noreferrer">
-        <LinkedIn className={classes.icon} />
+        {/* <LinkedIn className={classes.icon} /> */}
       </a>
       <a href="https://github.com/PalampalliPraneeth" target="_blank" rel="noopener noreferrer">
-        <GitHub className={classes.icon} />
+        {/* <GitHub className={classes.icon} /> */}
       </a>
       <a href="https://www.facebook.com/praneethkumarreddy.09?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
-        <Facebook className={classes.icon} />
+        {/* <Facebook className={classes.icon} /> */}
       </a>
       <a href="https://www.instagram.com/praneeth_ppkr" target="_blank" rel="noopener noreferrer">
-        <Instagram className={classes.icon} />
+        {/* <Instagram className={classes.icon} /> */}
       </a>
-    </div>
+      </div>
+    // </div>
   );
 };
 
